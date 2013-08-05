e0b20d138ec1238b1e9793c7a210331b0fd6d1b0
Velvel Marasow <velvel53@gmail.com>
2013-08-05 00:41:28 +0000

    Final versions of HW3 grader.js takes from a url with the --url flag and checks has all the checks required for HW3

--- grader.js
+++ grader.js
@@ -22,12 +22,13 @@ References:
 
 var fs = require('fs');
 var program = require('commander');
+var rest = require('restler');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
-    var instr =infile.toString();
+    var instr = infile.toString();
     if(!fs.existsSync(instr)) {
 	console.log("%s does not exist. Exiting.", instr);
 	process.exit(1); //http://nodejs.org/api/process.html#process_process_exit_code
@@ -37,6 +38,24 @@ var assertFileExists = function(infile) {
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var cheerioURL = function(url, checksfile) {
+    rest.get(url).on('complete', function(result) {
+	if (result instanceof Error) {
+	    console.log("%s does not exist. Exiting." + result.message, url);
+	    process.exit(1);
+	} else {
+	    $ = cheerio.load(result);	    
+	    var checks = loadChecks(checksfile).sort();
+	    var out = {};
+	    for(var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+	    }
+	    proccessJson(out);
+	}
+    });
 };
 
 var loadChecks = function(checksfile) {
@@ -50,8 +69,8 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     for(var ii in checks) {
 	var present = $(checks[ii]).length > 0;
 	out[checks[ii]] = present;
-    };
-    return out;
+    }
+    proccessJson(out);
 };
 
 var clone = function(fn) {
@@ -60,14 +79,18 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var proccessJson = function(rawJson) {
+    var outJson = JSON.stringify(rawJson, null, 4);
+    console.log(outJson);
+};
+
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('F, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	.option('-u, --url <url>', 'URL to html')
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.parse(process.argv);
+    (program.url) ? cheerioURL(program.url, program.checks) : checkHtmlFile(program.file, program.checks);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
